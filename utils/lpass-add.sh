525800502d00f435f02543c537b73df7dcbb4af1
Mike Pruett <mikepruett3@gmail.com>
2021-01-12 07:08:07 -0600

    More script changes

--- utils/lpass-add.sh
+++ utils/lpass-add.sh
@@ -7,7 +7,7 @@ function lpass-add () {
     # Check for LastPass CLI tool
     if ! hash lpass2; then
         echo "lpass not installed or included in path!"
-        exit
+        exit 3
     fi
 
     # Load in each SSH Private Key into ssh-agent
