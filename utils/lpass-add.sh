f4bd57f25527bfdb701c622ed59b6921ed8f8c9f
Mike Pruett <mikepruett3@gmail.com>
2021-01-12 07:04:08 -0600

    Moved script to function

--- utils/lpass-add.sh
+++ utils/lpass-add.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-# Setup Variables
-#ScriptName=$(basename $0)
-Container="SSH Keys"
+function lpass-add () {
+    # Setup Variables
+    Container="SSH Keys"
 
-# Check for LastPass CLI tool
-if ! hash lpass; then
-    echo "lpass not installed or included in path!"
-fi
+    # Check for LastPass CLI tool
+    if ! hash lpass2; then
+        echo "lpass not installed or included in path!"
+        exit 1
+    fi
 
-# Load in each SSH Private Key into ssh-agent
-for KEY in $(lpass ls "$Container" | awk '{ print substr( $2, 6, length($2) ) }'); do
-    lpass show --field="Private Key" "$Container/$KEY" | setsid ssh-add /dev/stdin
-done
+    # Load in each SSH Private Key into ssh-agent
+    for KEY in $(lpass ls "$Container" | awk '{ print substr( $2, 6, length($2) ) }'); do
+        lpass show --field="Private Key" "$Container/$KEY" | setsid ssh-add /dev/stdin
+    done
+}
