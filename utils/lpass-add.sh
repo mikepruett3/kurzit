5d332e3ecb94c09eb4c2a8d0be348fc73a4b6283
Mike Pruett <mikepruett3@gmail.com>
2021-01-12 06:56:02 -0600

    Added LastPass SSH Key Script

---
+++ utils/lpass-add.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Setup Variables
+ScriptName=$(basename $0)
+Container="SSH Keys"
+
+# Check for LastPass CLI tool
+if ! hash lpass; then
+    echo "lpass not installed or included in path!"
+fi
+
+# Load in each SSH Private Key into ssh-agent
+for KEY in $(lpass ls "$Container" | awk '{ print substr( $2, 6, length($2) ) }'); do
+    lpass show --field="Private Key" "$Container/$KEY" | setsid ssh-add /dev/stdin
+done
