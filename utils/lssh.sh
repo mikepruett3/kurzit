419890808603f0b207a2e544789d19a35c9ad6f2
Mike Pruett <mikepruett3@gmail.com>
2021-01-21 16:05:26 -0600

    More lssh testing

--- utils/lssh.sh
+++ utils/lssh.sh
@@ -27,8 +27,8 @@ function lssh () {
     # Load in each SSH Private Key into ssh-agent
     for KEY in $(lpass ls "$Container" | awk '{ print substr( $2, 6, length($2) ) }'); do
         if [[ "$Host" == "$KEY" ]]; then
-            PrivateKey=(lpass show --field="Private Key" "$Container/$KEY")
-            PublicKey=(lpass show --field="Public Key" "$Container/$KEY")
+            PrivateKey=lpass show --field="Private Key" "$Container/$KEY"
+            PublicKey=lpass show --field="Public Key" "$Container/$KEY"
             echo $PrivateKey
             echo $PublicKey
             #lpass show --field="Private Key" "$Container/$KEY" | setsid ssh-add /dev/stdin
