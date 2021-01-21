58a1163052b3374c6b04ee50436d7df7bc13d07f
Mike Pruett <mikepruett3@gmail.com>
2021-01-21 12:05:12 -0600

    Even more testing

--- utils/lssh.sh
+++ utils/lssh.sh
@@ -18,18 +18,19 @@ function lssh () {
     fi
 
     # Check for matching Host entries in ~/.ssh/config, and use the FQDN Hostname for $Host
-    if [[ -e "~/.ssh/config" ]]; then
+    if [ -e "~/.ssh/config" ]; then
         echo "SSH Config file found!"
-        if [[] $(grep "Host $Host" ~/.ssh/config) -ne "" ]]; then
-            Host=$(ssh -G $Host | grep -m1 -oP "(?<=hostname ).*")
-            echo $Host
-        fi
+        #if [[] $(grep "Host $Host" ~/.ssh/config) -ne "" ]]; then
+        #    Host=$(ssh -G $Host | grep -m1 -oP "(?<=hostname ).*")
+        #    echo $Host
+        #fi
     fi
+
     # Load in each SSH Private Key into ssh-agent
-    for KEY in $(lpass ls "$Container" | awk '{ print substr( $2, 6, length($2) ) }'); do
-        if [[ $Host -eq $KEY ]]; then
-            lpass show --field="Private Key" "$Container/$KEY" | setsid ssh-add /dev/stdin
-            ssh $Host
-        fi
-    done
+    #for KEY in $(lpass ls "$Container" | awk '{ print substr( $2, 6, length($2) ) }'); do
+    #    if [[ $Host -eq $KEY ]]; then
+    #        lpass show --field="Private Key" "$Container/$KEY" | setsid ssh-add /dev/stdin
+    #        ssh $Host
+    #    fi
+    #done
 }
