bf0b75f3dd4d4c6631470632b9cc960c7c1a73fa
Mike Pruett <mikepruett3@gmail.com>
2021-01-21 12:16:48 -0600

    DOH! == not !=

--- utils/lssh.sh
+++ utils/lssh.sh
@@ -27,7 +27,7 @@ function lssh () {
     # Load in each SSH Private Key into ssh-agent
     for KEY in $(lpass ls "$Container" | awk '{ print substr( $2, 6, length($2) ) }'); do
         echo $KEY
-        if [[ "$Host" != "$KEY" ]]; then
+        if [[ "$Host" == "$KEY" ]]; then
             echo "Match!"
             lpass show --field="Private Key" "$Container/$KEY" | setsid ssh-add /dev/stdin
             ssh $Host
